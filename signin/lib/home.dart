25f873bebb51cf40eca045087f339724960e909e
priyanka7777777 <sarafpriyanka434@gmail.com>
2022-08-08 23:29:45 +0545

    commit

--- signin/lib/home.dart
+++ signin/lib/home.dart
@@ -9,60 +9,39 @@ class MyLogin extends StatefulWidget{
         @override
       
       Widget build(BuildContext context){
-        return Container(
-          decoration: BoxDecoration(
-            image:DecorationImage(image:AssetImage('assets/assets.png'),fit:BoxFit.cover
-           ) ),
-
-
-
-
-child:Container(
-  padding:EdgeInsets.only(
-    top:MediaQuery.of(context).size.height*0.5,
-    right:35,
-    left:35),
-  child:Column(
-    children:[
-      TextField(
-        decoration:InputDecoration(
-          fillColor:Colors.grey.shade100,
-          filled:true,
-          hintText:'Welcome ' ,
-        border:OutlineInputBorder(
-          borderRadius:BorderRadius.circular(10) )
-     ) ),
-  
-  SizedBox(
-       height:40,
-       ),
-    Row(
-      mainAxisAlignment:MainAxisAlignment.spaceBetween,
-     children:[
-      TextButton(onPressed:(){
-        Navigator.pushNamed(context,'Signout');
-      },
-      child:Text('Sign Up', 
-      style:TextStyle(
-        decoration:TextDecoration.underline,
-        fontSize:28,
-        color:Color(0xff4c505b),
-  
-     
-
+        return Scaffold(
+          backgroundColor:Colors.grey[200],
+        appBar:AppBar(
+        title: Text("Home "),
+     ) ,
+        body:Center(
+            child:Card(
+              child:Column(
+              children:<Widget>[Image.asset("assets/asset.jpg")] ,)
+              
+            ),
         ),
-      ))
-     ],
-    )
+        );
+        
   
-],
-  ),
-  ),
+              
+              
+             
+            
+          
+              
+
+          
+        
+        
+
+       
+       
     
     
         
       
-      );
+      
       
       }
   }  
