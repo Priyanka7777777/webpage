c6a18a3c21352b0700ed45faa589fd04bdb363c2
priyanka7777777 <sarafpriyanka434@gmail.com>
2022-08-08 17:52:43 +0545

    commit

--- signin/lib/main.dart
+++ signin/lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
  
-void main() => runApp(const MyApp());
+void main() => runApp( const MyApp());
  
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class MyApp extends _MyStatefulWidgetState {
+ 
  
   static const String _title = 'Flutter App';
 
