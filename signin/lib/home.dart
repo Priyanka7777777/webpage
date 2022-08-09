22f55c46bbd5efe18fd2d21aecfd12234f0c70f2
priyanka7777777 <sarafpriyanka434@gmail.com>
2022-08-09 12:00:11 +0545

    commit

--- signin/lib/home.dart
+++ signin/lib/home.dart
@@ -14,10 +14,52 @@ class MyLogin extends StatefulWidget{
         appBar:AppBar(
         title: Text("Home "),
      ) ,
-        body:Center(
-            child:Card(
+        body:Padding(padding: const EdgeInsets.all(16.0),
+            child:SingleChildScrollView(
               child:Column(
-              children:<Widget>[Image.asset("assets/asset.jpg")] ,)
+              children:<Widget>[Image.asset("asset.jpg",fit: BoxFit.cover,),
+            SizedBox(
+              height:20,
+            ),
+             Text("Hello, How are You?",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold), 
+             ),
+              
+             SizedBox(
+              height:20,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextField(
+                decoration:
+                InputDecoration(hintText: "Enter something here"),
+
+              ),
+
+            ),
+             
+            Row(
+              children: <Widget>[
+               const Text('I want to go out from Homepage?'),
+                TextButton(
+                  child: const Text(
+                    'Sign up',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                              context,
+                              MaterialPageRoute(builder: (context) =>MyApp() ),
+                     );
+                    //signup screen
+                  },
+                )
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
+          ],
+
+
+               ),
               
             ),
         ),
